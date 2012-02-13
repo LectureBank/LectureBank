b0476aed246d1db69da2f8e8783a0db96415f5f4
Gregory Dracoulis <dracoulis@lecturebank.org>
2012-02-13 15:17:44 +0000

    Added "Get Satisfaction" (getsatisfaction.com) feedback widget to side of page.

--- footer.php
+++ footer.php
@@ -3,5 +3,30 @@
     <a href="/tos.php">Terms of Service</a> | <a href="/privacy.php">Privacy Policy</a> | <a href="mailto:admin@lecturebank.org">Contact Us</a>
   </footer>
 <!-- end #container --></div>
+<!-- code for "Get Satisfaction" feedback widget -->
+<script type="text/javascript" charset="utf-8">
+  var is_ssl = ("https:" == document.location.protocol);
+  var asset_host = is_ssl ? "https://s3.amazonaws.com/getsatisfaction.com/" : "http://s3.amazonaws.com/getsatisfaction.com/";
+  document.write(unescape("%3Cscript src='" + asset_host + "javascripts/feedback-v2.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
+<script type="text/javascript" charset="utf-8">
+  var feedback_widget_options = {};
+
+  feedback_widget_options.display = "overlay";  
+  feedback_widget_options.company = "lecturebank";
+  feedback_widget_options.placement = "left";
+  feedback_widget_options.color = "#222";
+  feedback_widget_options.style = "idea";
+  
+  
+  
+  
+  
+  
+  
+
+  var feedback_widget = new GSFN.feedback_widget(feedback_widget_options);
+</script>
 </body>
 </html>
